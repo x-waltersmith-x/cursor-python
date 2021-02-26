2c9081758ef351d94f153ad5c9f5f5d3e7e65fa4
wsmith <vk.iblogic@gmail.com>
2021-02-26 08:58:51 +0200

    HW3-4

--- format.py
+++ format.py
@@ -1,8 +1,10 @@
-# 5, 6, 7, 8, 9, 10 tasks
+# 5, 6, 7, 8, 9, 10, 11 tasks
 
 apples = "eleven"
 peaches = "seven"
+fruits = {"apples": apples, "peaches": peaches}
 
 
 print(f"Anna has {apples:.5s} apples and {peaches:.3s} peaches.".format(apples = apples, peaches = peaches))
 print(f"Anna has %s apples and %s peaches." % (apples, peaches))
+print(f"Anna has {fruits['apples']} apples and {fruits['peaches']} peaches.")

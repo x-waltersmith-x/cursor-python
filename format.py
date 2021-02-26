3f067b26f5ab9f7e655cf5f03ec184abb354f389
wsmith <vk.iblogic@gmail.com>
2021-02-26 08:32:03 +0200

    HW3-1

---
+++ format.py
@@ -0,0 +1,5 @@
+fruits = {'apples': 10000, 'peaches': 100}
+str = "Anna has {apples} apples and {peaches} peaches.".format(**fruits)
+
+
+print(str)
---
+++ id.py
@@ -0,0 +1,12 @@
+int_a = 55
+str_b = 'cursor'
+set_c = {1, 2, 3}
+lst_d = [1, 2, 3]
+dict_e = {'a': 1, 'b': 2, 'c': 3}
+
+
+print(type(int_a))
+print(type(str_b))
+print(type(set_c))
+print(type(lst_d))
+print(type(dict_e))

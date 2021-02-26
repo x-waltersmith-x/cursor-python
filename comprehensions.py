919015bd3dd295b5171949bb6a1744dfaaf3a84c
wsmith <vk.iblogic@gmail.com>
2021-02-26 19:19:50 +0200

    HW3-6

--- comprehensions.py
+++ comprehensions.py
@@ -1,4 +1,4 @@
-# 12
+# 12, 13, 14, 15
 
 
 # (1)
@@ -9,39 +9,58 @@ for num in range(10):
     else:
         lst.append(num ** 4)
 
-convert_lst = [num ** 2 if num % 2 == 1 else num ** 4 for num in range(10)]
-print(convert_lst)
+convert_list = [num ** 2 if num % 2 == 1 else num ** 4 for num in range(10)]
+print(convert_list)
 
 
 # (2)
 list_comprehension = [num // 2 if num % 2 == 0 else num * 10 for num in range(10)]
-convert_comprehension = []
+convert_comp = []
 for num in range(10):
     if num % 2 == 0:
-        convert_comprehension.append(num // 2)
+        convert_comp.append(num // 2)
     else:
-        convert_comprehension.append(num * 10)
+        convert_comp.append(num * 10)
 
-print(convert_comprehension)
+print(convert_comp)
+
 
 # (3)
 d = {}
 for num in range(1, 11):
     if num % 2 == 1:
         d[num] = num ** 2
-print(d)
+
+d_comp = {num: num ** 2 for num in range(1, 11)}
+print(d_comp)
+
 
 # (4)
-d = {}
+d_2 = {}
 for num in range(1, 11):
     if num % 2 == 1:
-        d[num] = num ** 2
+        d_2[num] = num ** 2
     else:
-        d[num] = num // 0.5
-print(d)
+        d_2[num] = num // 0.5
+d_2_comp = {num: num ** 2 if num % 2 == 1 else num // 0.5 for num in range(1, 11)}
+print(d_2_comp)
+
 
 # (5)
 dict_comprehension = {x: x**3 for x in range(10) if x**3 % 4 == 0}
+dict_reg = {}
+for x in range(10):
+    if x**3 % 4 == 0:
+        dict_reg[x] = x**3
+print(dict_reg)
 
-# (6)
-dict_comprehension = {x: x**3 if x**3 % 4 == 0 else x for x in range(10)}
+
+# # (6)
+dict_2_comprehension = {x: x**3 if x**3 % 4 == 0 else x for x in range(10)}
+dict_2_reg = {}
+for x in range(10):
+    if x**3 % 4 == 0:
+        dict_2_reg[x] = x**3
+    else:
+        dict_2_reg[x] = x
+print(dict_2_reg)

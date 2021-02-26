03ea1e1b2bfbb303ad43c79e4e7485a233b92d07
wsmith <vk.iblogic@gmail.com>
2021-02-26 20:41:29 +0200

    HW3-0

--- id.py
+++ id.py
@@ -6,6 +6,11 @@ set_c = {1, 2, 3}
 lst_d = [1, 2, 3]
 dict_e = {'a': 1, 'b': 2, 'c': 3}
 
+print(id(int_a))
+print(id(str_b))
+print(id(set_c))
+print(id(lst_d))
+print(id(dict_e))
 
 print(type(int_a))
 print(type(str_b))

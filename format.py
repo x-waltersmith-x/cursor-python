41c50737d411621978bcfb3de073bc6b9b9ba2b3
wsmith <vk.iblogic@gmail.com>
2021-03-01 08:08:55 +0200

    HW3-9-Ref

--- format.py
+++ format.py
@@ -5,14 +5,14 @@ peaches = "seven"
 fruits = {"apples": apples, "peaches": peaches}
 
 # 5
-print("Anna has {} apples and {} peaches.".format(11, 7))
+print("5) Anna has {} apples and {} peaches.".format(11, 7))
 # 6
-print("Anna has {0} apples and {1} peaches.".format(apples, peaches))
+print("6) Anna has {0} apples and {1} peaches.".format(apples, peaches))
 # 7
-print("Anna has {apples} apples and {apples} peaches.".format(apples = apples, peaches = peaches))
+print("7) Anna has {apples} apples and {apples} peaches.".format(apples=apples, peaches=peaches))
 # 8
-print("Anna has {0:.5} apples and {1:.3} peaches.".format(apples, peaches))
+print("8) Anna has {0:.5} apples and {1:.3} peaches.".format(apples, peaches))
 # 9, 10 tasks
-print(f"Anna has %s apples and %s peaches." % (apples, peaches))
+print(f"9-10) Anna has %s apples and %s peaches." % (apples, peaches))
 # 11
-print("Anna has {apples} apples and {peaches} peaches.".format(**fruits))
+print("11) Anna has {apples} apples and {peaches} peaches.".format(**fruits))

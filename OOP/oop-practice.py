c2fb206f5c97817a96c9ba6e05006879720438af
wsmith <vk.iblogic@gmail.com>
2021-03-17 22:58:17 +0200

    HW6-1

---
+++ OOP/oop-practice.py
@@ -0,0 +1,19 @@
+import dataclasses
+from abc import ABC, abstractmethod
+
+VEGETABLES = ["Cocktail", 'Cherry']
+FRUITS = ["Winter", 'Green']
+
+states = {'nothing': 0, 'flowering': 1, 'green': 2, 'red': 3, 'rotten': 4}
+
+
+class GardenMetaClass(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
